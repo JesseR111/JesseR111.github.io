100d562c6f7ce140bd9e9e7ea17a7fa4a653758c
JesseR111 <144633971+JesseR111@users.noreply.github.com>
2023-09-10 16:43:19 -0700

    Add files via upload

---
+++ treasurehunt.js
@@ -0,0 +1,15 @@
+// create a function called treasure
+const treasure = (location) => {
+    document.getElementById(location).innerHTML = "&#x1f332" 
+}
+var bomb = Math.floor(Math.random() * 8)
+var gold = Math.floor(Math.random() * 8)
+if (location===bomb) {
+    document.getElementById(location).innerHTML = "&#x2620"
+}
+else if (location===gold) {
+    document.getElementById(location).innerHTML = "&#x1f308"
+}
+else{
+    document.getElementById(location).innerHTML = "&#x1f332"  
+}
